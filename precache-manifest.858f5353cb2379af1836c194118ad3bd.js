49837d1522b7ae04d6f3e748753b5f767af487f7
csabagaspar <gaspar.csaba@gmail.com>
2019-03-18 10:17:08 +0100

    Updates

---
+++ precache-manifest.858f5353cb2379af1836c194118ad3bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  },
+  {
+    "revision": "93afddaf98bf5cd10fa2",
+    "url": "/pomodoro-timer/static/js/main.a3073d6a.chunk.js"
+  },
+  {
+    "revision": "21d847190828b89d8420",
+    "url": "/pomodoro-timer/static/js/2.28385a3f.chunk.js"
+  },
+  {
+    "revision": "d4e1e5e709db64168b1ad1da2469812c",
+    "url": "/pomodoro-timer/index.html"
+  }
+];
