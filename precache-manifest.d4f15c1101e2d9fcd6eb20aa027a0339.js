bf36927928871e87cf61556a7e2c36edab120ee4
csabagaspar <gaspar.csaba@gmail.com>
2019-04-24 21:29:54 +0200

    Updates

---
+++ precache-manifest.d4f15c1101e2d9fcd6eb20aa027a0339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  },
+  {
+    "revision": "1f3bd78a6e931d8e21d1",
+    "url": "/pomodoro-timer/static/js/main.ba77917d.chunk.js"
+  },
+  {
+    "revision": "111ecb3c431a4ae2d97d",
+    "url": "/pomodoro-timer/static/js/2.a1f2d3a2.chunk.js"
+  },
+  {
+    "revision": "111ecb3c431a4ae2d97d",
+    "url": "/pomodoro-timer/static/css/2.63424ef6.chunk.css"
+  },
+  {
+    "revision": "bd1e0076fbf666058fa8b212bdcbad6d",
+    "url": "/pomodoro-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-timer/precache-manifest.5103da5501578161321f7a00dab11e01.js"
+  "/pomodoro-timer/precache-manifest.d4f15c1101e2d9fcd6eb20aa027a0339.js"
 );
 
 workbox.clientsClaim();
