9af44d31b8cdffda0af3d9d3e5facc546faf5e27
csabagaspar <gaspar.csaba@gmail.com>
2019-05-12 11:52:18 +0200

    add raw add and remove impl

--- src/screens/main/store.js
+++ src/screens/main/store.js
@@ -1,25 +1,28 @@
 import React, {useState} from 'react'
 
-const defaultItems = [
-  {
-    title: 'todo 1',
-    estimated: 3,
-    completed: 0,
-  },
-  {
-    title: 'todo 2',
-    estimated: 2,
-    completed: 1,
-    expanded: true,
-    children: [
-      {
-        title: 'todo 2.1',
-        estimated: 4,
-        completed: 0,
-      },
-    ],
-  },
-]
+const defaultItems = {
+  treeData: [
+    {
+      title: 'todo 1',
+      estimated: 3,
+      completed: 0,
+    },
+    {
+      title: 'todo 2',
+      estimated: 2,
+      completed: 1,
+      expanded: true,
+      children: [
+        {
+          title: 'todo 2.1',
+          estimated: 4,
+          completed: 0,
+        },
+      ],
+    },
+  ],
+  addAsFirstChild: false,
+}
 
 export const ItemContext = React.createContext()
 
--- src/screens/main/todo.js
+++ src/screens/main/todo.js
@@ -1,6 +1,10 @@
 import React, {useContext} from 'react'
 import 'react-sortable-tree/style.css'
-import SortableTree, {changeNodeAtPath} from 'react-sortable-tree'
+import SortableTree, {
+  changeNodeAtPath,
+  addNodeUnderParent,
+  removeNodeAtPath,
+} from 'react-sortable-tree'
 import {ItemContext} from './store'
 
 export function TodoTree() {
@@ -8,10 +12,21 @@ export function TodoTree() {
 
   const getNodeKey = ({treeIndex}) => treeIndex
 
+  const update = (field, value, node, path) => {
+    setItems(items => ({
+      treeData: changeNodeAtPath({
+        treeData: items.treeData,
+        path,
+        getNodeKey,
+        newNode: {...node, [field]: value},
+      }),
+    }))
+  }
+
   return (
     <SortableTree
-      treeData={items}
-      onChange={items => setItems(items)}
+      treeData={items.treeData}
+      onChange={treeData => setItems({treeData})}
       generateNodeProps={({node, path}) => ({
         title: (
           <>
@@ -20,14 +35,14 @@ export function TodoTree() {
               value={node.title}
               onChange={event => {
                 const title = event.target.value
-                setItems(items =>
-                  changeNodeAtPath({
-                    treeData: items,
+                setItems(items => ({
+                  treeData: changeNodeAtPath({
+                    treeData: items.treeData,
                     path,
                     getNodeKey,
                     newNode: {...node, title},
                   }),
-                )
+                }))
               }}
             />
             <input
@@ -36,19 +51,54 @@ export function TodoTree() {
               type="number"
               onChange={event => {
                 const estimated = event.target.value
-                setItems(items =>
-                  changeNodeAtPath({
-                    treeData: items,
+                setItems(items => ({
+                  treeData: changeNodeAtPath({
+                    treeData: items.treeData,
                     path,
                     getNodeKey,
                     newNode: {...node, estimated},
                   }),
-                )
+                }))
               }}
             />
             <label style={{fontSize: '1.1rem'}}>{node.completed}</label>
+            {!node.children && <label>"selectable"</label>}
           </>
         ),
+        buttons: [
+          <button
+            onClick={() =>
+              setItems(items => ({
+                treeData: addNodeUnderParent({
+                  treeData: items.treeData,
+                  parentKey: path[path.length - 1],
+                  expandParent: true,
+                  getNodeKey,
+                  newNode: {
+                    title: `new`,
+                    estimated: 0,
+                  },
+                  addAsFirstChild: items.addAsFirstChild,
+                }).treeData,
+              }))
+            }
+          >
+            Add Child
+          </button>,
+          <button
+            onClick={() =>
+              setItems(items => ({
+                treeData: removeNodeAtPath({
+                  treeData: items.treeData,
+                  path,
+                  getNodeKey,
+                }),
+              }))
+            }
+          >
+            Remove
+          </button>,
+        ],
       })}
     />
   )
