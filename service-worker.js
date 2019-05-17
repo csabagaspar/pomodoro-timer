1a3e8647614a9152b607f5d061d95cdfb5810bce
csabagaspar <gaspar.csaba@gmail.com>
2019-05-17 09:53:00 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-timer/precache-manifest.a06f023edcfe6087a35d473fa78a0c48.js"
+  "/pomodoro-timer/precache-manifest.7b92cd09f9efe75bbaa5e0938669cedb.js"
 );
 
 workbox.clientsClaim();
