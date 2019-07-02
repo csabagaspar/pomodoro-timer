29de1a1cf2af212e043f111cead74a0023107dd1
csabagaspar <gaspar.csaba@gmail.com>
2019-07-02 16:40:50 +0200

    refact

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import Main from './layouts/main'
 function App() {
   return <Main />
 }
+console.log('pub', process.env.PUBLIC_URL)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.register()
